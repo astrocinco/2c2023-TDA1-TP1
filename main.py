437338b19c7003872cd932c56df65d144dd24631
tin-gprieto <mgonzalezp@fi.uba.ar>
2023-09-08 16:00:08 -0300

    feat: se implementa un generador de dataset segun n rivales, se encapsula el analisis general de una solucion

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def get_optimal_analisis_order(teams_list):
     optimal_analisis_order = sorted(teams_list, key= lambda team:team[1], reverse=True)
     return optimal_analisis_order
@@ -11,27 +13,40 @@ def aux_get_analisis_duration(analisis_order):
         scaloni_wait_time += team[0]
     return team_analisis_max_duration
 
-def main(teams_list = []):
-    teams_list = [
-        # Tuplas, primero con el tiempo de analisis de Scaloni y luego del ayudante. TODO: No hardcodear
-        (1, 3),
-        (5, 1),
-        (4, 8),
-        (4, 3),
-        (1, 5),
-        (2, 9),
-        (2, 2),
-        (2, 4),
-        (1, 6),
-        (6, 5),
-    ]
-    return get_optimal_analisis_order(teams_list)
+def make_teams_list(n_rivals):
+    teams_list = []
+    for i in range(n_rivals):
+        teams_list.append((random.randint(0, n_rivals+10),  random.randint(0, n_rivals+10)))
+    return teams_list
 
-print("Hello world! ★★★")
-optimal_order = main()
-print("Optimal order is:", optimal_order)
-print("Analisis time is:", aux_get_analisis_duration(main()))
+def get_solution_analysis(teams_list, time_expected=-1):
+    print(f"Solution for a team list of {len(teams_list)} rivals")
+    solution = get_optimal_analisis_order(teams_list)
+    print("Optimal order is:", solution)
+    print("Analysis time is:", aux_get_analisis_duration(solution))
+    print(f"Time expected: {time_expected}")
+    
+def main():
+    teams_list_test = [
+            # Tuplas, primero con el tiempo de analisis de Scaloni y luego del ayudante. TODO: No hardcodear
+            (1, 3),
+            (5, 1),
+            (4, 8),
+            (4, 3),
+            (1, 5),
+            (2, 9),
+            (2, 2),
+            (2, 4),
+            (1, 6),
+            (6, 5),
+        ]
 
+    print("Hello world! ★★★")
+    get_solution_analysis(teams_list_test)
+    print("--------------------------------------------------------")
+    get_solution_analysis(make_teams_list(15))
+
+main()
 
 
 """ (3, 3),
