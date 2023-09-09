2c3bdd334ef5aec8b0802ae9f97a769d03574a5c
cmtFIUBA <115751549+cmtFIUBA@users.noreply.github.com>
2023-09-09 11:09:13 -0300

    data set txt main.py

--- main.py
+++ main.py
@@ -4,18 +4,17 @@ DATA_SET_FILE_PATH = "casos_prueba_catedra/"
 def get_data_set_from_txt(file):
     data_set = []
     file = DATA_SET_FILE_PATH + file
-    i=0
-    for line in open(file):
-        if i!=0:
+
+    with open(file,'r') as o_file:
+        
+        o_file.readline()
+        
+        for line in o_file:
             aux_list = line.split(",")
-            tupla = (int(aux_list[0]), int(aux_list[1].rstrip()))
-            data_set.append(tupla)
-        i+=1
-    
+            l_tuple = (int(aux_list[0]), int(aux_list[1].rstrip()))
+            data_set.append(l_tuple)
+
     return data_set
-
-
-
 
 
 def get_optimal_analisis_order(teams_list):
@@ -31,22 +30,6 @@ def aux_get_analisis_duration(analisis_order):
         scaloni_wait_time += team[0]
     return team_analisis_max_duration
 
-#def main(teams_list = []):
- #   teams_list = [
-        # Tuplas, primero con el tiempo de analisis de Scaloni y luego del ayudante. TODO: No hardcodear
-  #      (1, 3),
-   #     (5, 1),
-    #    (4, 8),
-     #   (4, 3),
-      #  (1, 5),
-       # (2, 9),
-       # (2, 2),
-       # (2, 4),
-       # (1, 6),
-       # (6, 5),
-    #]
-    #return get_optimal_analisis_order(teams_list)
-
 def main():
 
     teams_list = get_data_set_from_txt('10000-elem.txt')
@@ -57,23 +40,3 @@ def main():
 print("Hello world! ★★★")
 optimal_order = main()
 print(aux_get_analisis_duration(optimal_order))
-
-
-
-
-""" (3, 3),
-(5, 1),
-(1, 8), """
-
-
-
-""" (1, 3),
-(5, 1),
-(4, 8),
-(4, 3),
-(1, 5),
-(2, 9),
-(2, 2),
-(2, 4),
-(1, 6),
-(6, 5), """
