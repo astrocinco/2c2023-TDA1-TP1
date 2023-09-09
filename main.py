aa0419b9e1263ba734f8d04a88cd07826485182e
tin-gprieto-zenbook <matingp@proton.me>
2023-09-09 13:27:33 -0300

    feat: se quitan funciones obsoletas de sets

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-import random
+from sets import *
 
-DATA_SET_FILE_PATH = "casos_prueba_catedra/"
-
-def get_data_set_from_txt(file):
-    data_set = []
-    file = DATA_SET_FILE_PATH + file
-
-    with open(file,'r') as o_file:
-        
-        o_file.readline()
-        
-        for line in o_file:
-            aux_list = line.split(",")
-            l_tuple = (int(aux_list[0]), int(aux_list[1].rstrip()))
-            data_set.append(l_tuple)
-    return data_set
+CATEDRA_DATA_SET_FILE_PATH = "./casos_prueba_catedra/"
+SCALONI_ANALYSIS = 0
+ASISTENT_ANALYSIS = 1
 
 def get_optimal_analisis_order(teams_list):
     optimal_analisis_order = sorted(teams_list, key= lambda team:team[1], reverse=True)
@@ -24,30 +12,32 @@ def aux_get_analisis_duration(analisis_order):
     team_analisis_max_duration = 0
     scaloni_wait_time = 0
     for team in analisis_order:
-        if scaloni_wait_time + team[0] + team[1] > team_analisis_max_duration:
-            team_analisis_max_duration = scaloni_wait_time + team[0] + team[1]
-        scaloni_wait_time += team[0]
+        scaloni_wait_time += team[SCALONI_ANALYSIS]
+        if scaloni_wait_time + team[ASISTENT_ANALYSIS] > team_analisis_max_duration:
+            team_analisis_max_duration = scaloni_wait_time + team[ASISTENT_ANALYSIS]
     return team_analisis_max_duration
 
-def make_teams_list(n_rivals):
-    teams_list = []
-    for i in range(n_rivals):
-        teams_list.append((random.randint(0, n_rivals+10),  random.randint(0, n_rivals+10)))
-    return teams_list
-
-def get_solution_analysis(teams_list, time_expected=-1):
+def get_solution_analysis(teams_list, time_expected=0, show_solution=False):
     print(f"Solution for a team list of {len(teams_list)} rivals")
     solution = get_optimal_analisis_order(teams_list)
-    print("Optimal order is:", solution)
+    if(show_solution): print("Optimal order is:", solution)
     print("Analysis time is:", aux_get_analisis_duration(solution))
-    print(f"Time expected: {time_expected}")
-    
-def main():
-    
-    teams_list_test = teams_list = get_data_set_from_txt('10000-elem.txt')
-    print("Hello world! ★★★")
-    get_solution_analysis(teams_list_test)
+    if(time_expected): print(f"Time expected: {time_expected}")
     print("--------------------------------------------------------")
-    get_solution_analysis(make_teams_list(15))
+    
+def get_catedra_data_sets(path):
+    data_sets=[]
+    catedra_test_files=['3-elem.txt','10-elem.txt','100-elem.txt','10000-elem.txt']
+    for file in catedra_test_files:
+        teams_list = get_data_set_from_txt(path, file)
+        data_sets.append((teams_list, get_optimal_time_from_txt(path, len(teams_list))))
+    return data_sets 
 
+def main():
+
+    print("Hello world! ★★★")
+    for data_set in get_catedra_data_sets(CATEDRA_DATA_SET_FILE_PATH):
+        get_solution_analysis(data_set[0], data_set[1])
+    get_solution_analysis(get_random_teams_list(15))
+    
 main()
