40485c4cb62e636802de4e273dbbdf00ab72b23e
tin-gprieto-zenbook <matingp@proton.me>
2023-09-16 11:52:26 -0300

    feat: se agrega ejecucion para archivos externos

--- solutions.py
+++ solutions.py
@@ -1,3 +1,6 @@
+import sys
+import sets
+
 SCALONI_INDEX = 0
 ASSISTANT_INDEX = 1
 
@@ -37,3 +40,48 @@ def get_analisis_duration(analisis_order):
         if scaloni_wait_time + team[ASSISTANT_INDEX] > team_analisis_max_duration:
             team_analisis_max_duration = scaloni_wait_time + team[ASSISTANT_INDEX]
     return team_analisis_max_duration
+
+
+'''
+argv[1] -> file
+arv[2] -> flag
+flags:
+    - -a : alternative solution
+    - -r : random solution
+    - -b : brute force solution
+    - -o : optimal solution
+
+'''
+
+def determinate_method(flag):
+    match flag:
+        case "-a":
+            return ("alternative", get_alternative_analisis_order)
+        case "-r":
+            return ("random", get_random_analisis_order)
+        case "-b":
+            return ("brute force", get_brute_force_analisis_order)
+        case _:
+            return ("optimal", get_optimal_analisis_order)
+        
+if __name__ == "__main__":
+    try:
+        file = sys.argv[1]
+    except IndexError:
+        print("ERROR")
+        print("Usage: <arg1> file with information of videos, <arg2> flags for determinate method")
+        sys.exit(1)
+        
+    try:
+        flag = sys.argv[2]
+    except IndexError:
+        flag = ""
+        
+    (method_name, method) = determinate_method(flag)
+
+    teams_list = sets.get_data_set_from_txt(file)
+    analysis_order = method(teams_list)
+    
+    print(f"Order by {method_name} method: {analysis_order}")
+    print(f"Total duration: {get_analisis_duration(analysis_order)}")
+    
