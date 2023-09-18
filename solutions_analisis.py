1a79520ce551bc72407d39611d108d4afd494f6a
tin-gprieto-zenbook <matingp@proton.me>
2023-09-18 13:14:56 -0300

    feat: se quitan funciones sin utilizar. Se agrega descripcion a las funciones

--- solutions_analisis.py
+++ solutions_analisis.py
@@ -1,45 +1,9 @@
 import solutions
 import sets
 
-import time as t
 import timeit
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
-
-def compare_solutions(data_set_size, amount):
-    for i in range(amount):
-        teams_list = sets.get_random_teams_list(data_set_size)
-        # main.get_analisis_duration(main.get_optimal_analisis_order(teams_list)) 
-        duration_analisis_optimo = solutions.get_analisis_duration(solutions.get_optimal_analisis_order(teams_list))
-        duration_analisis_no_optimo = solutions.get_analisis_duration(solutions.get_alternative_analisis_order(teams_list))
-        duration_analisis_random = solutions.get_analisis_duration(solutions.get_random_analisis_order(teams_list))
-
-        #print(f"OPTIMO : {analisis_optimo}, NO OPTIMO : {no_optimo}, RANDOM : {analisis_random}")
-        if duration_analisis_no_optimo < duration_analisis_optimo or duration_analisis_random < duration_analisis_optimo:
-            print(f"FALLó EL ANALISIS ..... OPTIMO : {duration_analisis_optimo}, NO OPTIMO : {duration_analisis_no_optimo}, RANDOM : {duration_analisis_random}. DATA SET {teams_list}")
-            break
-    print("todo salio bien!")
-
-
-def random_graph():
-    list_x=[]
-    y_rand=[]
-    data_size = 5
-    teams_list = sets.get_random_teams_list(data_size)
-    y_opt = []
-    for i in range(200):
-        list_x.append(i)    
-        y_opt.append(solutions.get_analisis_duration(solutions.get_optimal_analisis_order(teams_list)))
-        y_rand.append( solutions.get_analisis_duration(solutions.get_random_analisis_order(teams_list)))    
-    fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')  # Create a figure containing a single axes.
-    ax.plot(list_x,y_opt, label="optimo")
-    ax.plot(list_x ,y_rand,label="random")
-
-    ax.legend()
-    plt.show()
-    
 
 def solution_analysis(teams_list, solution_method=solutions.get_optimal_analisis_order):
     solution = solution_method(teams_list)
